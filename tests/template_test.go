e9296c63084a58048c605f19399aef10883cf3f8
Alex Robson <asrobson@gmail.com>
2022-09-19 18:59:51 -0600

    fix: move sql templates into binary build using embed directive (#3)

--- tests/template_test.go
+++ tests/template_test.go
@@ -1,19 +1,29 @@
 package tests
 
 import (
+	"embed"
 	"strings"
 	"testing"
 
 	"github.com/arobson/spry/storage"
 )
 
+//go:embed sql
+var sqlFiles embed.FS
+
 func TestCreateTemplate(t *testing.T) {
 	type Data struct {
 		ActorName string
 	}
-	result, err := storage.CreateFromTemplate(
+	templates, err := storage.CreateTemplateFromFS(
+		sqlFiles,
+		"sql/create_actor_schema.sql",
+	)
+	if err != nil {
+		t.Error("failed to create templates from embedded FS", err)
+	}
+	result, err := templates.Execute(
 		"create_actor_schema.sql",
-		"../postgres/sql/create_actor_schema.sql",
 		Data{ActorName: strings.ToLower("TesT")},
 	)
 	if err != nil {
